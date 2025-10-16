475543032d057ec62a53aa23152f3f47a735b24f
Gustavo Fernando <gugamefernando@gmail.com>
2025-10-16 10:55:01 -0300

    Improve search parameters. Adding second test file.

--- R/search_genes.R
+++ R/search_genes.R
@@ -5,7 +5,7 @@
 buscar_rnai <- function(tabela, coluna) {
   #Lista de genes da maquinaria
   categorias <- list(
-   "Dicer family" = c("DCR1","RNC"),
+   "Dicer family" = c("DCR1","DCR2","RNC"),
    "Argonautes" = c("AGO1","AGO2","AGO3","PIWI","AUB","SPNE"),
    "dsRNA binding" = c("LOQS","DGCR8"),
    "dsRNA transport/uptake" = c("SID1","SID2","CLH","INX2","EPN","SCRB1"),
@@ -22,7 +22,9 @@ buscar_rnai <- function(tabela, coluna) {
   #Procurando correspondências
   resultados <- lapply(names(categorias), function(cat) {
     padroes <- categorias[[cat]]
-    linhas <- grepl(paste(padroes, collapse = "|"), col_data, ignore.case = TRUE)
+    padroes_esc <- vapply(padroes, function(x) paste0("\\Q", x, "\\E"), FUN.VALUE = "")
+    pattern <- paste0("(^|[^A-Za-z0-9])(", paste(padroes_esc, collapse = "|"), ")([^A-Za-z0-9]|$)")
+    linhas <- grepl(pattern, col_data, ignore.case = TRUE, perl = TRUE)
     subset <- tabela[linhas, , drop = FALSE]
 
     if (nrow(subset) == 0) return(NULL)  # <- ignora categorias sem resultados
@@ -31,6 +33,6 @@ buscar_rnai <- function(tabela, coluna) {
     return(subset)
   })
   # Unir tudo em um único data frame
-  resultado_final <- do.call(rbind, resultados)
-  return(resultado_final)
+  resultado_final <- dplyr::select(do.call(rbind, resultados), "X.gene_id","sprot_Top_BLASTX_hit","Categoria")
+    return(resultado_final)
 }
--- tests/testthat/test_script.R
+++ tests/testthat/test_script.R
@@ -2,10 +2,12 @@
 
 # Chamando tabela 1 para testes
 library (readxl)
-  arquivo <- system.file("extdata", "tabela.teste1.xls", package = "rnaiMachinerySearch")
+  arquivo <- system.file("extdata", "tabela.teste2.xls", package = "rnaiMachinerySearch")
   tabela <- read.table(arquivo,
                       sep = "\t",
                       header = TRUE,
                       quote = "",
                       comment.char = "",
                       fill = TRUE)
+# Testando a busca
+resultado <- buscar_rnai(tabela,"sprot_Top_BLASTX_hit")
