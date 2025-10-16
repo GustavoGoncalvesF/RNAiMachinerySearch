99a7ff635658baf12eb9af1f910307c4a9d8cdb2
Gustavo Fernando <gugamefernando@gmail.com>
2025-10-16 02:21:31 -0300

    Adjust in test files.

--- tests/testthat/test_script.R
+++ tests/testthat/test_script.R
@@ -1,6 +1,11 @@
 # Script utilizado para testar o pacote
 
 # Chamando tabela 1 para testes
-
+library (readxl)
   arquivo <- system.file("extdata", "tabela.teste1.xls", package = "rnaiMachinerySearch")
-  tabela <- readxl::read_excel(arquivo)
+  tabela <- read.table(arquivo,
+                      sep = "\t",
+                      header = TRUE,
+                      quote = "",
+                      comment.char = "",
+                      fill = TRUE)
