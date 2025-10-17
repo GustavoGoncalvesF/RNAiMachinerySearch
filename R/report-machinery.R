45d9feea2f95c08c4d860f51e33ec2ed18193ee7
Gustavo Fernando <gugamefernando@gmail.com>
2025-10-17 16:01:12 -0300

    Fixing bug with commits.

--- R/report-machinery.R
+++ R/report-machinery.R
@@ -2,7 +2,7 @@
 
 report.machinery <- function(tabela) {
 
-  #primeiro definir a coluna category como fator, para evitar que desorganize na contagem
+  #primeiro definir a coluna category como fator, para evitar que desorganize na contagems
   tabela$Category <- factor(tabela$Category, levels = unique(tabela$Category))
   contagem <- dplyr::count(tabela, Category, sort=FALSE)
   cat("Relatório de genes da maquinaria de RNAi encontrados:\n", "Total global: ", nrow(tabela), "genes.\n", "Dicer family: ", contagem[1,2], "genes.\n", "Argonautes: ", contagem[2,2], "genes.\n", "dsRNA binding: ", contagem[3,2], "genes.\n", "dsRNA transport/uptake: ", contagem[4,2], "genes.\n", "Auxiliary RISC-associated: ", contagem[5,2], "genes.")
