99866699dc0dc3c78e815c150ccf092c2d2a7cf0
Gustavo Fernando <gugamefernando@gmail.com>
2025-10-17 14:20:00 -0300

    Adding the function text in output. Moving the genes to search to gene_list.txt.

--- R/plot-sunburst.R
+++ R/plot-sunburst.R
@@ -1,10 +1,11 @@
 #Função que plota o sunburst dos genes encontrados
 library(plotly)
 
-plot_sunburst <- function(tabela){
+sunburst.plot <- function(tabela){
     data <- data.frame(
       labels = c(unique(tabela$Category), tabela$ProteinAnnotation),
-      parents = c(rep("", length(unique(tabela$Category))), tabela$Category)
+      parents = c(rep("", length(unique(tabela$Category))), tabela$Category),
+      hovertext = c(rep("", length(unique(tabela$Category))), tabela$Function)
     )
 return(
     plot_ly(
@@ -12,7 +13,9 @@ return(
       labels = ~labels,
       parents = ~parents,
       type = "sunburst",
-      branchvalues = "total"
+      branchvalues = "total",
+      hovertext = ~hovertext,
+      hoverinfo = "text+label"
     ) %>%
       layout(title = "Distribuição de genes por categoria")
 )
