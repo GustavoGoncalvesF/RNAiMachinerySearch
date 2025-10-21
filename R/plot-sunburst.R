9b85ce3e29a37eb70cd55cbb073c34af9580827e
Gustavo Fernando <gugamefernando@gmail.com>
2025-10-20 23:59:27 -0300

    Adding heatmap ploting function, fix some tabulation in general.

--- R/plot-sunburst.R
+++ R/plot-sunburst.R
@@ -3,21 +3,21 @@
 library(plotly)
 
 sunburst.plot <- function(rnai_hits){
-    data <- data.frame(
-      labels = c(unique(rnai_hits$Category), rnai_hits$ProteinAnnotation),
-      parents = c(rep("", length(unique(rnai_hits$Category))), rnai_hits$Category),
-      hovertext = c(rep("", length(unique(rnai_hits$Category))), rnai_hits$Function)
+  data <- data.frame(
+  labels = c(unique(rnai_hits$Category), rnai_hits$ProteinAnnotation),
+  parents = c(rep("", length(unique(rnai_hits$Category))), rnai_hits$Category),
+  hovertext = c(rep("", length(unique(rnai_hits$Category))), rnai_hits$Function)
     )
-return(
-    plot_ly(
-      data,
-      labels = ~labels,
-      parents = ~parents,
-      type = "sunburst",
-      branchvalues = "total",
-      hovertext = ~hovertext,
-      hoverinfo = "text+label"
-    ) %>%
+    return(
+      plot_ly(
+        data,
+        labels = ~labels,
+        parents = ~parents,
+        type = "sunburst",
+        branchvalues = "total",
+        hovertext = ~hovertext,
+        hoverinfo = "text+label"
+      ) %>%
       layout(title = "Distribution of genes by categories")
 )
 }
