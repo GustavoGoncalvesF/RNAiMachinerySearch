45955b9c1554e017e8e172e669e3c61f2674df94
Gustavo Fernando <gugamefernando@gmail.com>
2025-10-20 23:59:27 -0300

    Altered test script.

---
+++ R/plot-heatmap.R
@@ -0,0 +1,15 @@
+# This function plots a heatmap to represent founded RNAi core genes expression amoung samples
+
+library(ComplexHeatmap)
+
+heatmap.plot <- function(rnai_hits, expr_matrix){
+  # Merge keeping only protein annotation's present in the expressio matrix, and preserve they order
+  gene_present <- rnai_hits$GeneID[rnai_hits$GeneID %in% rownames(expr_matrix)]
+  heatmap_data <- expr_matrix[gene_present, , drop = FALSE]
+  rownames(heatmap_data) <- rnai_hits$ProteinAnnotation[rnai_hits$GeneID %in% gene_present]
+
+  # Normalizing data before plot
+  heatmap_data_z <- t(scale(t(as.matrix(heatmap_data))))
+
+  ComplexHeatmap::Heatmap(heatmap_data_z, name = "Z-score", row_names_side = "left", cluster_columns = FALSE, row_names_gp = gpar(fontsize = 10))
+}
--- tests/testthat/test_script.R
+++ tests/testthat/test_script.R
@@ -1,25 +1,24 @@
 # Script used to test the execution flow of the package
 
 # Input full table
-  file <- file.path("C:/Users/gugam/Documents/Data/mahanarva_annotation_report.xls")
-  file <- file.path("C:/Users/gugam/Documents/Data/centropomus_annotation_report.tsv")
-  file <- file.path("C:/Users/gugam/Documents/Data/dugesia_annotation_report.xls")
+file1 <- file.path("C:/Users/gugam/Documents/Data/mahanarva_annotation_report.xls")
+file1 <- file.path("C:/Users/gugam/OneDrive/Documentos/Data/mahanarva_annotation_report.xls")
+file1 <- file.path("C:/Users/gugam/Documents/Data/centropomus_annotation_report.tsv")
+file1 <- file.path("C:/Users/gugam/Documents/Data/dugesia_annotation_report.xls")
 
 # Input partial tables
-  file <- system.file("extdata", "partial_table1.xls", package = "rnaiMachinerySearch")
-  annotation_df <- read.table(file,
-                      sep = "\t",
-                      header = TRUE,
-                      quote = "",
-                      comment.char = "",
-                      fill = TRUE)
+file1 <- system.file("extdata", "partial_table2.xls", package = "rnaiMachinerySearch")
+annotation_df <- read.table(file1, sep = "\t", header = TRUE, quote = "", comment.char = "", fill = TRUE)
+
+# Input expression matrix
+file2 <- file.path("C:/Users/gugam/Documents/Data/raw_counts_filtered.txt")
+expr_matrix <- read.table(file2, header = TRUE, row.names = 1, sep = " ", stringsAsFactors = FALSE)
 
 # Testing search function
 rnai_hits <- search.rnai(annotation_df,"sprot_Top_BLASTX_hit")
 
-# Testing report print function
-report.machinery(rnai_hits)
-
 # Testing sunburst plot function
 sunburst.plot(rnai_hits)
 
+# Testins heatmap plot function
+heatmap.plot(rnai_hits, expr_matrix)
