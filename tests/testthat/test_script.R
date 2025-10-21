e9e9ef119f7bfb5f7f4c119ba5e8b67181b28dae
Gustavo Gonçalves <gugamefernando@gmail.com>
2025-10-21 09:57:14 -0300

    Add initial content to README.md

--- tests/testthat/test_script.R
+++ tests/testthat/test_script.R
@@ -1,6 +1,6 @@
 # Script used to test the execution flow of the package
 
-# Input full tables
+# Input full table
 file1 <- file.path("C:/Users/gugam/Documents/Data/mahanarva_annotation_report.xls")
 file1 <- file.path("C:/Users/gugam/OneDrive/Documentos/Data/mahanarva_annotation_report.xls")
 file1 <- file.path("C:/Users/gugam/Documents/Data/centropomus_annotation_report.tsv")
